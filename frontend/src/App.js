ce60a02ea66815b4b26eef80f1159af8703f3c61
ottosmaniotto <rodrigosmaniotto@gmail.com>
2017-11-14 19:10:58 -0200

    react 2nd edition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var company = {
+    name: 'Planvooore',
+    location: 'Florianópolis',
+    business: 'dashboards'
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    } else {
+        return undefined;
+    }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1 className="app-title">{company.name}</h1>
+          {getLocation(company.location)}
+              <p className="">
+              {company.business ? company.business : 'test'}
+              </p>
         </header>
-        <p className="App-intro">
-          To get ssssdsadsatarted, edit <code>src/App.js</code> and save to reload.
+        <p className="app-intro">
+          This is a pilot React application.
         </p>
       </div>
     );
   }
 }
+
+export default App;
